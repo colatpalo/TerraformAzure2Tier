72b5b497575bbf58e2289cb403cb320295a4b59a
colatpalo <71885206+colatpalo@users.noreply.github.com>
2021-10-18 17:48:12 +0100

    Delete azure_provider.tf

--- azure_provider.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-    version = "=2.13.0"
-    features {}
-    subscription_id = "9a3eb5f8-75bc-48c1-b7c5-238802a9842d"
-    client_id = "6bb83dcc-084a-4ad8-9c89-c5b345b6bccc"
-    client_secret = "hMmRqeKKu2.wcd1UEtZgX58_bvwAlLc3w-"
-    tenant_id = "66b66353-3b76-4e41-9dc3-fee328bd400e"
-}
